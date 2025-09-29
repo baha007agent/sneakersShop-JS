0c094a76d55a8583837bacb8237e2d8e388d58f9
amanturkubanychbekov <kubanycbekovamantur2@gmail.com>
2025-09-30 01:21:11 +0300

    Finished counter

--- js/scritpt.js
+++ js/scritpt.js
@@ -2,16 +2,26 @@ const btnPlus = document.querySelector('[data-action="plus"]')
 const btnMinus = document.querySelector('[data-action="minus"]')
 const counter = document.querySelector('[data-counter]')
 
-console.log(counter);
+window.addEventListener("click", (event) => {
 
-btnPlus.addEventListener("click", () => {
-  counter.innerText = ++counter.innerText
-})
+  let counter;
 
-btnMinus.addEventListener("click", () => {
-  if (parseInt(counter.innerText) === 1) {
-    counter.innerText = 1
-  } else {
-    counter.innerText = --counter.innerText
+  if (event.target.dataset.action === "plus" || event.target.dataset.action === "minus") {
+    const counterWrapper = event.target.closest(".counter-wrapper")
+    counter = counterWrapper.querySelector('[data-counter]')
+
   }
+
+  if (event.target.dataset.action === "plus") {
+
+    counter.innerText = ++counter.innerText
+  }
+
+  if (event.target.dataset.action === "minus") {
+
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText
+    }
+  }
+
 })
